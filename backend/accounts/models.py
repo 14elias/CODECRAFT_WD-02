d7b310830038a22081b85ae3c75fd1c52584e8cf
14elias <ellamebrahtom1995@gmail.com>
2025-04-09 20:40:54 +0300

    model updated

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User,AbstractUser
-
+from django.contrib.auth.models import User
 
 class Employee(models.Model):
     first_name = models.CharField(max_length=100)
@@ -12,6 +11,7 @@ class Employee(models.Model):
     date_hired = models.DateField()
     is_active = models.BooleanField(default=True)
     phone_number = models.CharField(max_length = 10)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
