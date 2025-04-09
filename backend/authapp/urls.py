346e7b019d17c0c897ca1d451f26a594846d38d7
14elias <ellamebrahtom1995@gmail.com>
2025-04-09 23:46:47 +0300

    logout implemented

--- backend/authapp/urls.py
+++ backend/authapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import CustomTokenObtainPairView,CustomTokenRefreshView
+from .views import CustomTokenObtainPairView,CustomTokenRefreshView,LogoutView,isAuthenticated
 
 urlpatterns = [
     path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('is_authenticated/', isAuthenticated.as_view(), name='is_authenticated'),
 ]
