555971241689ea170c082ed800e8f9b0702b53c6
14elias <ellamebrahtom1995@gmail.com>
2025-04-10 19:59:31 +0300

    ...

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -5,10 +5,10 @@ class EmployeeSerializer(ModelSerializer):
     class Meta:
         model = Employee
         fields=['id','first_name','last_name','email','department','position','salary','date_hired','phone_number','created_by']
-        read_only_fields=['created_by']
+        read_only_fields=['created_by','date_hired','id']
     
     def create(self,validated_data):
         user=self.context['request'].user
         employee=Employee.objects.create(created_by = user,**validated_data)
-        employee.save()
         return employee
+    
