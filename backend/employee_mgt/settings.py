be725d2851a05080897f335dc120fe22344c871c
14elias <ellamebrahtom1995@gmail.com>
2025-04-11 19:40:59 +0300

    cors header

--- backend/employee_mgt/settings.py
+++ backend/employee_mgt/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'accounts',
     'authapp',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,6 +133,12 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
 }
 
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
