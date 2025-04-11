4b985a83a706b8469f08eb6aee7bf91890b19ce2
14elias <ellamebrahtom1995@gmail.com>
2025-04-12 00:10:04 +0300

    ..

--- backend/employee_mgt/settings.py
+++ backend/employee_mgt/settings.py
@@ -130,7 +130,7 @@ REST_FRAMEWORK = {
     )
 }
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
 }
 
 CORS_ALLOW_CREDENTIALS = True
