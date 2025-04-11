3df8b094961e1252ad0c7f61735df839b7545fa7
14elias <ellamebrahtom1995@gmail.com>
2025-04-11 10:34:12 +0300

    ...

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import Login from "./routes/login"
+import Home from "./routes/home"
+import { Routes,Route } from 'react-router-dom'
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <ChakraProvider>
+      <Routes>
+        <Route path="login/" element={<Login/>}/>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </ChakraProvider>
   )
 }
 
