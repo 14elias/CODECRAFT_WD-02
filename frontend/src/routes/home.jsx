a9b6cd50fd4d659c27aecf227799a010adcb6221
14elias <ellamebrahtom1995@gmail.com>
2025-04-13 00:21:35 +0300

    ..

--- frontend/src/routes/home.jsx
+++ frontend/src/routes/home.jsx
@@ -1,24 +1,25 @@
-import { Box, Heading } from "@chakra-ui/react";
-import NavBar from "../components/navbar";
+import React from 'react';
+import { Box, Text, VStack } from '@chakra-ui/react';
+import EmployeeSidebar from '../components/EmployeeSidebar';
 
-function Home() {
-    return (
-        <>
-        <NavBar/>
-        <Box
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            height="100vh"
-            bgGradient="linear(to-r, teal.400, blue.500)"
-            color="white"
-        >
-            <Heading as="h1" size="2xl" textAlign="center">
-                Hello Everyone
-            </Heading>
-        </Box>
-        </>
-    );
-}
+const Home = () => {
+  return (
+    <Box>
+      {/* Top Navigation Bar */}
+      <EmployeeSidebar />
+
+      {/* Main Content */}
+      <Box p={5} bgGradient="linear(to-r, teal.400, blue.500)" color="white" minH="100vh">
+        <Text fontSize="2xl" fontWeight="bold" mb={5}>
+          Welcome Employee
+        </Text>
+        <VStack spacing={4} align="start">
+          <Text fontSize="lg">This is the home page for employees.</Text>
+          <Text fontSize="lg">Use the navigation bar above to access different sections.</Text>
+        </VStack>
+      </Box>
+    </Box>
+  );
+};
 
 export default Home;
